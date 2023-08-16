aca3dab746262b4695a62df339ad44a43008bb1d
Michel Lutegar <mlutegar@gmail.com>
2023-08-16 15:58:56 -0300

    feat: adicionando exceção de exclusão

--- AC2/AC2.go
+++ AC2/AC2.go
@@ -2,24 +2,24 @@ package main
 
 import "fmt"
 
-type Contato struct{
-	nome string
+type Contato struct {
+	nome  string
 	email string
 }
 
 // alterarEmail: função que altera o email do contato
-func (c *Contato) alterarEmail(emailNovo string){
+func (c *Contato) alterarEmail(emailNovo string) {
 	c.email = emailNovo
 }
 
-func main()  {
+func main() {
 	var listaContato [5]Contato
 
 	// Interface por linha de comando que permite a inclusão ou exclusão de contatos.
 	var op int
 	op = 100
 
-	for op != 0{
+	for op != 0 {
 		fmt.Println("\n\nCONTATOS")
 		fmt.Println("Escolha o que quer fazer: (0 para sair, 1 para adicionar, 2 para excluir último contato)")
 
@@ -49,9 +49,9 @@ func main()  {
 }
 
 // adicionaContato: recebe um contato e um array com até 5 elementos e inclui o contato no primeiro elemento vazio do array.
-func adicionaContato(nomeContato string, emailContato string, lista [5]Contato) [5]Contato{
+func adicionaContato(nomeContato string, emailContato string, lista [5]Contato) [5]Contato {
 
-	for contatoId,contatoValue := range lista{
+	for contatoId, contatoValue := range lista {
 		if (contatoValue == Contato{}) {
 			lista[contatoId] = Contato{nome: nomeContato, email: emailContato}
 			break
@@ -62,13 +62,17 @@ func adicionaContato(nomeContato string, emailContato string, lista [5]Contato) 
 }
 
 // excluiContato, que recebe um array de 5 elementos e elimina o último contato válido.
-func excluiContato(lista [5]Contato) [5]Contato{
-	for contatoId,contatoValue := range lista{
-		if (contatoValue == Contato{}) {
-			lista[contatoId-1] = Contato{}
-			break
+func excluiContato(lista [5]Contato) [5]Contato {
+	if (lista[4] != Contato{}) {
+		lista[4] = Contato{}
+	} else {
+		for contatoId, contatoValue := range lista {
+			if (contatoValue == Contato{}) {
+				lista[contatoId-1] = Contato{}
+				break
+			}
 		}
 	}
+
 	return lista
 }
-
