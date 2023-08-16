1c49c4aac7a4938ea3e5acbcc91be3167c635ea2
Michel Lutegar <mlutegar@gmail.com>
2023-08-16 08:02:27 -0300

    feat: aula 3

---
+++ 07-structs/structArray.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Pessoa struct{
+	Nome string
+}
+
+func main()  {
+	var p = Pessoa{
+		Nome: "Michel",
+	}
+
+	var pessoas[3]Pessoa
+
+	pessoas[0] = p
+
+	for _,pessoa := range pessoas{
+		if (pessoa != Pessoa{}) {
+			fmt.Println(pessoa)
+		}
+	}
+}
